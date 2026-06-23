ca74251832136f8159b17cf5a5f100c2d27e80e5
adrianwarth8-create <adrianwarth8@gmail.com>
2026-06-23 14:37:37 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIZ5nDYZWOabPWsikUJm06qk0hg_uObE8",
+    authDomain: "sirmed3gacap.firebaseapp.com",
+    projectId: "sirmed3gacap",
+    storageBucket: "sirmed3gacap.firebasestorage.app",
+    messagingSenderId: "529913526905",
+    appId: "1:529913526905:web:6edbec562fa8324f11f2a8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
