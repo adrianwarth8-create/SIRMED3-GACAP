ce660494faac292b0d91de9a8298d6383ccd339e
adrianwarth8-create <adrianwarth8@gmail.com>
2026-06-23 14:40:05 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,72 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCIZ5nDYZWOabPWsikUJm06qk0hg_uObE8",
-    authDomain: "sirmed3gacap.firebaseapp.com",
-    projectId: "sirmed3gacap",
-    storageBucket: "sirmed3gacap.firebasestorage.app",
-    messagingSenderId: "529913526905",
-    appId: "1:529913526905:web:6edbec562fa8324f11f2a8"
-  };
+import {
+getAuth,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+updateDoc,
+doc,
+query,
+where,
+orderBy,
+serverTimestamp
+}
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyCIZ5nDYZWOabPWsikUJm06qk0hg_uObE8",
+
+authDomain: "sirmed3gacap.firebaseapp.com",
+
+projectId: "sirmed3gacap",
+
+storageBucket: "sirmed3gacap.firebasestorage.app",
+
+messagingSenderId: "529913526905",
+
+appId: "1:529913526905:web:6edbec562fa8324f11f2a8"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+window.auth = auth;
+window.db = db;
+
+window.signInWithEmailAndPassword =
+signInWithEmailAndPassword;
+
+window.signOut = signOut;
+
+window.onAuthStateChanged =
+onAuthStateChanged;
+
+window.collection = collection;
+window.addDoc = addDoc;
+window.getDocs = getDocs;
+window.deleteDoc = deleteDoc;
+window.updateDoc = updateDoc;
+window.doc = doc;
+window.query = query;
+window.where = where;
+window.orderBy = orderBy;
+window.serverTimestamp =
+serverTimestamp;
+
+console.log("🏥 SIRMED conectado");
